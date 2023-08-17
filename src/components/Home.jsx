d08e3c9011a180297f2367b3adaf9bbb3c46b6a7
Nihar Phansalkar <phansalkarnihar@gmail.com>
2023-08-17 15:59:34 +0000

    completed navbar and added a QR code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import QRCode from 'react-qr-code';
+import '../styles/homeStyles.css';
 
-function Home() {
+function Home({ qrValue }) {
   return (
-    <div />
+    <div id="home">
+      <QRCode
+        style={{ background: 'white', padding: '16px' }}
+        value={qrValue}
+      />
+    </div>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { HashLink } from 'react-router-hash-link';
 import '../styles/navStyles.css';
+import logo from '../assets/codex_dark.png';
 
 function Navbar() {
   return (
-    <nav className="container">
-      <img src="" alt="Logo" />
+    <nav>
+      <img src={logo} alt="Logo" />
       <main>
         <HashLink to="/#home">Home</HashLink>
         <HashLink to="/#about">About</HashLink>

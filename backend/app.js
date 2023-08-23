22cfdcf9c28161b2eba9db399beae360a7661d8a
Nihar Phansalkar <phansalkarnihar@gmail.com>
2023-08-23 12:25:12 +0000

    removed eslint and serving frontend via server

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "/frontend")));
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const app = require("./app");
 const db = require("./db/db");
 const teamRoute = require("./routes/teamsRoute");
+const path = require("path");
 
 app.use("/api/v1", teamRoute);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "index.html"));
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on http://localhost:${process.env.PORT}`);
